2123d5708f2ab2cc5ac744d888f7360c5b020f75
Otso Saarinen <saarinenotso@gmail.com>
2025-09-07 18:29:39 +0300

    create variables + form for creating a new datatransfer

--- NewFileTransferPage.xaml.cs
+++ NewFileTransferPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,12 +21,26 @@ namespace Pathway
 	/// </summary>
 	public partial class NewFileTransferPage : Page
 	{
+
+		public string SourceFile { get; set; }
+		public string SourceFolder { get; set; }
+		public string DestinationFolder { get; set; }
+
 		public NewFileTransferPage()
 		{
 			InitializeComponent();
 		}
 		private void BackToHome_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
+			NavigationService.Navigate(new HomePage());
+		}
+
+		private void AddFileTransfer_Click(object sender, RoutedEventArgs e)
+		{
+			SourceFile = SourceFileTextBox.Text;
+			SourceFolder = SourceFolderTextBox.Text;
+			DestinationFolder = DestinationFolderTextBox.Text;
+
 			NavigationService.Navigate(new HomePage());
 		}
 	}
